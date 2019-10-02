63da8c049c3a07e4a068b2f2d30a3817ccce65a8
Maddytec <maddytec@gmail.com>
2019-10-02 07:17:23 -0300

    Viewing pages with security auth

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Router } from '@angular/router';
+import { SharedService } from '../services/shared.service';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router) {}
 
-    canActivate() {
-        if (localStorage.getItem('isLoggedin')) {
+    shared: SharedService;
+
+    constructor(private router: Router) {
+        this.shared = SharedService.getInstance();
+    }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean> | boolean {
+
+        if (this.shared.isLoggedIn()) {
             return true;
         }
 
