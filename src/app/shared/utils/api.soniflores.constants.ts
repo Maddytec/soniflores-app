3a744a536fc55de63ffd027fc3381515ffabe64c
Maddytec <maddytec@gmail.com>
2019-10-25 06:20:51 -0300

    URLs API

--- src/app/shared/utils/api.soniflores.constants.ts
+++ src/app/shared/utils/api.soniflores.constants.ts
@@ -1,2 +1,6 @@
+export const API_SONIFLORES_LOCAL: string = 'http://localhost:4200';
+export const API_SONIFLORES_DEV: string = 'http://35.229.43.177/soniflores-api';
+export const API_SONIFLORES_PRODUCAO: string = 'http://35.229.43.177/soniflores-api'; 
+
+
 export const API_SONIFLORES: string = 'http://localhost:8080';
- 
