80179093c0a40486b7ded8fb3cce0cab139dff55
Maddytec <maddytec@gmail.com>
2019-10-31 08:44:49 -0300

    Ajuste na constante das API

--- src/app/shared/services/grupo.service.ts
+++ src/app/shared/services/grupo.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../model/user.model';
-import { API_SONIFLORES } from '../utils/api.soniflores.constants';
+import { API } from '../utils/api.constants';
 import { Grupo } from '../model/grupo.model';
 
 @Injectable()
@@ -11,26 +10,26 @@ export class GrupoService {
 
   createOrUpdate(grupo: Grupo){
     if(grupo.id != null && grupo.id != ''){
-      return this.http.put(`${API_SONIFLORES}/grupo/${grupo.id}`, grupo);
+      return this.http.put(`${API}/grupo/${grupo.id}`, grupo);
     } else {
       grupo.id = null;
-      return this.http.post(`${API_SONIFLORES}/grupo`, grupo);
+      return this.http.post(`${API}/grupo`, grupo);
     }
   }
 
   findAllLazy(page: number, size: number ){
-    return this.http.get(`${API_SONIFLORES}/grupo/lazy?page=${page}&size=${size}`)
+    return this.http.get(`${API}/grupo/lazy?page=${page}&size=${size}`)
   }
 
   findAll(){
-    return this.http.get(`${API_SONIFLORES}/grupo`)
+    return this.http.get(`${API}/grupo`)
   }
 
   findById(id: string){
-    return this.http.get(`${API_SONIFLORES}/grupo/${id}`)
+    return this.http.get(`${API}/grupo/${id}`)
   }
 
   delete(id: string){
-    return this.http.delete(`${API_SONIFLORES}/grupo/${id}`)
+    return this.http.delete(`${API}/grupo/${id}`)
   }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../model/user.model';
-import { API_SONIFLORES } from '../utils/api.soniflores.constants';
+import { API } from '../utils/api.constants';
 
 @Injectable()
 export class UserService {
@@ -9,27 +9,27 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   login(user: User): any {
-    return this.http.post(`${API_SONIFLORES}/usuario/login`, user);
+    return this.http.post(`${API}/usuario/login`, user);
   }
 
   createOrUpdate(user: User){
     if(user.id != null && user.id != ''){
-      return this.http.put(`${API_SONIFLORES}/usuario/${user.id}`, user);
+      return this.http.put(`${API}/usuario/${user.id}`, user);
     } else {
       user.id = null;
-      return this.http.post(`${API_SONIFLORES}/usuario`, user);
+      return this.http.post(`${API}/usuario`, user);
     }
   }
 
   findAll(page: number, size: number ){
-    return this.http.get(`${API_SONIFLORES}/usuario/v2?page=${page}&size=${size}`)
+    return this.http.get(`${API}/usuario/v2?page=${page}&size=${size}`)
   }
 
   findById(id: string){
-    return this.http.get(`${API_SONIFLORES}/usuario/${id}`)
+    return this.http.get(`${API}/usuario/${id}`)
   }
 
   delete(id: string){
-    return this.http.delete(`${API_SONIFLORES}/usuario/${id}`)
+    return this.http.delete(`${API}/usuario/${id}`)
   }
 }
