19a5f5486db3284c99937ecd4d52b772ddf3432e
Maddytec <maddytec@gmail.com>
2019-11-16 19:18:13 -0300

    Adicionado rota e module da edição de produto

---
+++ src/app/components/produto/produto-edicao/produto-edicao-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProdutoEdicaoComponent } from './produto-edicao.component';
+
+const routes: Routes = [
+    {
+        path: '', component: ProdutoEdicaoComponent
+    }
+
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class ProdutoEdicaoRoutingModule {
+}
---
+++ src/app/components/produto/produto-edicao/produto-edicao.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PageHeaderModule } from '../../../shared';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { 
+    MatTableModule, 
+    MatSortModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatPaginatorModule, 
+    MatIconModule } from '@angular/material';
+import { ProdutoEdicaoRoutingModule } from './produto-edicao-routing.module';
+import { ProdutoEdicaoComponent } from './produto-edicao.component';
+
+@NgModule({
+    imports: [
+        ReactiveFormsModule,
+        FormsModule, 
+        CommonModule,
+        ProdutoEdicaoRoutingModule,
+        PageHeaderModule,
+        CommonModule,
+        PageHeaderModule,
+        MatTableModule,
+        MatSortModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatPaginatorModule,
+        MatIconModule
+    ],
+    declarations: [ProdutoEdicaoComponent]
+})
+export class ProdutoEdicaoModule {}
